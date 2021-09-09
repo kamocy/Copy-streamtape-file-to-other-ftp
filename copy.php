cbac69123e9161552dab8fb9be3d51509a353cc7
KamocyTV <harisulumam@gmail.com>
2021-09-09 19:44:08 +0700

    Update copy.php

--- copy.php
+++ copy.php
@@ -1,10 +1,13 @@
 <?php
 function ftpcopy($uftp, $pftp, $nftp, $file) {
     if($nftp == 'VO') {
+        //voe.sx
         $sftp = 'ftp.voe-network.net';
     } elseif($nftp == 'SB') {
+        //streamsb.com
         $sftp = 'ftp.streamsb.com';
     } else {
+        //add more
         $sftp = '0';
     }
     //streamtape ftp account
@@ -17,5 +20,7 @@ function ftpcopy($uftp, $pftp, $nftp, $file) {
     }
     return $ret;
 }
+//file.mp4 same as filename in streamtape
+//VO another name for ftp.voe-network.net
 //other ftp account
 echo ftpcopy('user', 'pass', 'VO', 'file.mp4');
